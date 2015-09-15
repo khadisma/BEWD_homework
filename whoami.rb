07ad47fdb9b5d1a6abe38c6598c51a4484755534
Eddy Huang <khadisma@gmail.com>
2015-09-15 13:45:26 -0700

    who am i ruby file

---
+++ whoami.rb
@@ -0,0 +1,17 @@
+puts "What is your name?"
+
+name = gets.chomp
+
+puts "Where did you grow up?"
+
+home = gets.chomp
+
+puts "What is your current occupation?"
+
+occupation = gets.chomp
+
+puts "Any hidden talents?"
+
+talents = gets.chomp
+
+puts "My name is " + name + ", I grew up in " + home + ". " + "I currently work as a " + occupation + " and I " + talents + "."
