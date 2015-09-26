4a18060071d4b1c2145076415a7442dfa6f81be1
Eddy Huang <khadisma@gmail.com>
2015-09-26 09:01:29 -0700

    Added Grandma file

---
+++ class_3_4_homework/grandma.rb
@@ -0,0 +1,27 @@
+# puts "Ask Grandma a Question!"
+# question = gets.chomp
+
+
+
+while loop
+
+  puts "Ask Grandma a Question!"
+  question = gets.chomp
+
+
+  if question != question.upcase
+
+    puts "HUH?! SPEAK UP, SONNY!"
+
+  elsif question == "BYE"
+
+
+  else
+
+    puts "NO, NOT SINCE 1938!"
+
+    loop = false
+
+  end
+
+end
